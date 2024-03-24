cfaae054326a5a3245f7e1aa3fb0712e881ddab3
mirza zain <mirzazain269@gmail.com>
2024-03-25 00:21:26 +0500

    Updated

---
+++ main.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+const answer = await inquirer.prompt([
+    {
+        message: 'Guess the number from 1 - 10: ', name: 'guessthenum', type: 'number'
+    }
+]);
+const randomnum = Math.floor(Math.random() * 10);
+if (randomnum == answer.guessthenum) {
+    console.log(`You have guessed the right number!!,\n You input ${answer.guessthenum}\n Computer's input ${randomnum}`);
+}
+else {
+    console.log(`The number you guessed is wrong,\n You input ${answer.guessthenum}\n Computer's input ${randomnum}`);
+}
